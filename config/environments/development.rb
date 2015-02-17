f12e816d09c58d4bfd01fd17642faff15b103775
carlos eduardo gonçalves <carloseduardopmc@hotmail.com>
2015-02-17 20:08:56 -0300

    change production config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ CampnightUserEngine::Application.configure do
 
   # Mailer
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
